0059331e657b89d6c59e0841b3740a6881ee8579
sburnham3 <sburnham3@gmail.com>
2025-02-13 17:35:46 -0500

    add auto scenaio comments

--- src/main/java/frc/robot/commands/Pathautos/SinglePathFollowing.java
+++ src/main/java/frc/robot/commands/Pathautos/SinglePathFollowing.java
@@ -57,6 +57,40 @@ private PathPlannerPath path;
   public void execute() {
     //TODO command the this.swerve to drive the pathPlannerPath
     // swerve.drive();
+
+    /* parallel commands group 
+    *  1a. drivePath1 from BlueOne to reef 60-A position (AKA -startingTrajectory)
+    *  1b. coralPivot to ready to Top_coral_deploy (so elevator is free to move)
+    *  1c. algaePivot to unstowed                  (so elevator is free to move)     */
+    
+    // 2. elavator to Top_coral_deploy position
+    // 3. CoralDeploy 
+
+    /* parallel commands group 
+    * 4a. elevator goto retrieve_coral_position
+    * 4b. coralPivot to retrieve_coral_position  
+    * 4c. drivePath from reef 60-A to CoralSupply      */
+    
+    // 5. CoralIntake_and_Hold
+    
+    /* parallel commands
+    *  6a. drivePath from CoralSupply to reef 60b 
+    *  6b. coralPivot to ready to Top_coral_deploy 
+    *  6c. elavator raise to Top_coral_deploy position  */
+
+    // 7. CoralDeploy
+
+    /* parallel commands group 
+    * 8a. elevator goto retrieve_coral_position
+    * 8b. coralPivot to retrieve_coral_position  
+    * 8c. drivePath from reef 60-B to CoralSupply  */
+  
+    // 9. CoralIntake_and_Hold
+    
+    /* parallel commands
+    *  10a. drivePath from CoralSupply to reef 60b 
+    *  10b. coralPivot to ready to Top_coral_deploy 
+    *  10c. elavator to Top_coral_deploy position  */
   }
 
   // Called once the command ends or is interrupted.
