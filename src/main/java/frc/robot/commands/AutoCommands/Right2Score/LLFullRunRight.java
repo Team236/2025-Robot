f1e025d2f4cceda6de53807483df972f2731edf7
defiorem742 <mikedefiore742@gmail.com>
2025-02-18 17:03:45 -0500

    updated LLFullRunRight to move 1.6" from target at end to line up with Coral

--- src/main/java/frc/robot/commands/AutoCommands/Right2Score/LLFullRunRight.java
+++ src/main/java/frc/robot/commands/AutoCommands/Right2Score/LLFullRunRight.java
@@ -23,17 +23,27 @@ public class LLFullRunRight extends SequentialCommandGroup {
     addCommands(
       //TODO  add the commands for scoring and receiving coral
 
-          //LEG 1:
+          //LEG 1:  
+          //First command to drive with odometry and end 9" from bumper to AprilTag, centered on Tag
           new DriveFwdAndSideAndTurn(s_Swerve, false, 63.6, -85.35, -58).withTimeout(3),
+          //Use limelight to get exactly 12" from front frame (9" from bumper) to AprilTag
           new TargetAllParallel(s_Swerve,12, 0).withTimeout(2),
-          new DriveFwdAndSideAndTurn(s_Swerve, false, 9, -6, 0),
+          //Needs to end  with Limelight camera centered 1.6" to the right of the AprilTag center
+          new DriveFwdAndSideAndTurn(s_Swerve, false, 9, -1.6, 0),
         
           //LEG2:
+          //Tweak as needed to end against the Coral Loading station, not close to side of field
+          //note - camera on 2025 robot will be on opposite side of robot compared with the swerve testbed
           new DriveSideways(s_Swerve, false, 84).withTimeout(2),
           new DriveFwdAndSideAndTurn(s_Swerve, false, -15, 97, -68),
           //LEG3:
+          //First command to drive with odometry and end 9" from bumper to AprilTag, centered on Tag  
+        
           new DriveFwdAndSideAndTurn(s_Swerve, true,120, -13, 6).withTimeout(3.5),
-          new TargetAllParallel(s_Swerve, 12, 0).withTimeout(2)
+          //Use limelight to get exactly 12" from front frame (9" from bumper) to AprilTag
+          new TargetAllParallel(s_Swerve, 12, 0).withTimeout(2),
+          //Needs to end  with Limelight camera centered 1.6" to the right of the AprilTag center
+          new DriveFwdAndSideAndTurn(s_Swerve, false, 9, -1.6, 0)
 
     );
   
