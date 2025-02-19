e9e180e6cbaf5f0c47746b568eed1eded0731a33
Ellen DiCarlo <edicarlo@sbcglobal.net>
2025-02-19 17:25:08 -0500

    Created auto commands for Leg1andLeg2 combined

--- src/main/java/frc/robot/commands/AutoCommands/Left/Leg1Left.java
+++ src/main/java/frc/robot/commands/AutoCommands/Left/Leg1Left.java
@@ -5,14 +5,15 @@
 package frc.robot.commands.AutoCommands.Left;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Swerve;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class Leg1Left extends SequentialCommandGroup {
   /** Creates a new Leg1Left. */
-  public Leg1Left() {
-   
+  public Leg1Left(Swerve s_Swerve) {
+    //!!!!! TODO MAKE ALL Y DISTANCES AND ALL ANGLES OPPOSITE TO Right !!!!
     addCommands();
   }
 }
