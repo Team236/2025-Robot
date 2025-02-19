83a96cacf3017930f6b2f4cfe8c01a8c7bf559cd
Ellen DiCarlo <edicarlo@sbcglobal.net>
2025-02-19 14:36:04 -0500

    Added Get/ResetPoseWithLL to AlgaeTarget, CoralLTarget, CoralRTarget

--- src/main/java/frc/robot/commands/Targeting/CoralLeftTarget.java
+++ src/main/java/frc/robot/commands/Targeting/CoralLeftTarget.java
@@ -20,9 +20,14 @@ public class CoralLeftTarget extends SequentialCommandGroup {
   public CoralLeftTarget(Swerve s_Swerve) {
     addCommands(  
       new TargetAllParallel(s_Swerve, 12, 0).withTimeout(2.0),
-    //****TODO:  ADD COMMAND HERE TO RESET POSE WITH LIMELIGHT, BEFORE DRIVING WITH ODOMETRY:
-   // new ResetPoseWithLL(s_Swerve),
+
+      //****TODO:  ADD COMMAND HERE TO RESET POSE WITH LIMELIGHT, BEFORE DRIVING WITH ODOMETRY
+      // new GetPoseWithLL(s_Swerve),
+
       new DriveFwdAndSideAndTurn(s_Swerve, false, 10, 1.6, 0).withTimeout(3)); 
+  
+      //****TODO:  ADD COMMAND HERE TO RESET POSE TO VALUE FROM GetPoseWithLL
+      //, new ResetPoseWithLL(s_Swerve)
   }
 
 }
