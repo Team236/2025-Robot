54d3c888763d4465f00e63bbb8f7a52d35769e1a
Ellen DiCarlo <edicarlo@sbcglobal.net>
2025-02-19 17:25:40 -0500

    Added s_Swerve missing in Leg2Left

--- src/main/java/frc/robot/commands/AutoCommands/Left/Leg2Left.java
+++ src/main/java/frc/robot/commands/AutoCommands/Left/Leg2Left.java
@@ -5,14 +5,15 @@
 package frc.robot.commands.AutoCommands.Left;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Swerve;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class Leg2Left extends SequentialCommandGroup {
   /** Creates a new Leg2Left. */
-  public Leg2Left() {
-
+  public Leg2Left(Swerve s_Swerve) {
+ //!!!!! TODO MAKE ALL Y DISTANCES AND ALL ANGLES OPPOSITE TO Right !!!!
     addCommands();
   }
 }
