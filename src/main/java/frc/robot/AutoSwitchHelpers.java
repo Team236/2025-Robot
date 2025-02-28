f8acce7ce1babde17571bfc64d6095ad03436b71
Emmett Campbell <campbellemmett5@gmail.com>
2025-02-27 19:07:27 -0500

    added autoSwitchHelpers

---
+++ src/main/java/frc/robot/AutoSwitchHelpers.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.List;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class AutoSwitchHelpers
+{
+    private static DigitalInput autoSwitch1 = new DigitalInput(Constants.DIO_AUTO_1);
+    private static DigitalInput autoSwitch2 = new DigitalInput(Constants.DIO_AUTO_2);
+    private static DigitalInput autoSwitch3 = new DigitalInput(Constants.DIO_AUTO_3);
+    private static DigitalInput autoSwitch4 = new DigitalInput(Constants.DIO_AUTO_4);
+    
+    public boolean switchesAre(boolean a, boolean b, boolean c, boolean d)
+    {
+        if (autoSwitch1.get() == a && autoSwitch2.get() == b && autoSwitch3.get() == c && autoSwitch4.get() == d)
+        {
+            return true;
+        }
+        return false;
+    }
+}
