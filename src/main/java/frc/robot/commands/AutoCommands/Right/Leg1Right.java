659a10e7e193841a011fdd0ba1ee50dd2efe2820
Ellen DiCarlo <edicarlo@sbcglobal.net>
2025-03-26 08:00:27 -0400

    Updated comments at end of Leg1Right

--- src/main/java/frc/robot/commands/AutoCommands/Right/Leg1Right.java
+++ src/main/java/frc/robot/commands/AutoCommands/Right/Leg1Right.java
@@ -60,10 +60,11 @@ public class Leg1Right extends SequentialCommandGroup {
         //new ResetPoseWithLL(s_Swerve).withTimeout(0.25),
 
       new EndDriveTrajectoryPID(s_Swerve).withTimeout(0.5)
-       // new L4_Score(elevator, coralHold, coralPivot, algaePivot)
-
-      // Could use AutoLeg2 score, which does not bring elevator down - if bring it down at start of leg2
+    
+      //Use AutoLeg1 score, which does not bring elevator down - if bring it down at start of leg2
       //new L4_Score_AutoLeg1(elevator, coralHold, coralPivot, algaePivot)
+      //OTHERWISE USE:
+      // new L4_Score(elevator, coralHold, coralPivot, algaePivot)
     );     
 
   }
